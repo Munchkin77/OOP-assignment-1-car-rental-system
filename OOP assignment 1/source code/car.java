32e0a7875db6da45fe2712c2f0e05df09a950e70
Munchkin77 <nyawirasharon607@gmail.com>
2024-06-05 15:38:28 +0300

    Add files via upload

---
+++ OOP assignment 1/source code/car.java
@@ -0,0 +1,39 @@
+public class Car {
+    private String licensePlate;
+    private String make;
+    private String model;
+    private int year;
+    private boolean isAvailable;
+
+    public Car(String licensePlate, String make, String model, int year) {
+        this.licensePlate = licensePlate;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.isAvailable = true;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
